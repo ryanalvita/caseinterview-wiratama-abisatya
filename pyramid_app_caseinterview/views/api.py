fc781684c740a074bb685ece7429dffdac2e4437
Abisatya <abisatya90@gmail.com>
2024-12-15 12:27:30 +0000

    PRODUCT BACKLOG ITEM 8291 – Timeseries API: Add Date Filtering from URL Query Parameter

--- pyramid_app_caseinterview/views/api.py
+++ pyramid_app_caseinterview/views/api.py
@@ -9,6 +9,7 @@ from pyramid_app_caseinterview.models.timeseries import Timeseries
 from pyramid_app_caseinterview.views.serialization import DateObject
 
 from sqlalchemy import func
+from datetime import datetime
 
 from . import View
 
@@ -23,7 +24,27 @@ class API(View):
         request_method="GET",
     )
     def timeseries_api(self):
-        query = self.session.query(Timeseries)
+        start_date = self.request.params.get("from")
+        end_date = self.request.params.get("to")
+
+        try:
+            if start_date:
+                start_date =datetime.strptime(start_date, "%Y-%m-%d")
+            if end_date:
+                end_date =datetime.strptime(end_date, "%Y-%m-%d")
+        except ValueError:
+            return {"Error" : "Invalid date format. use YYYY-MM-DD."}
+        
+        
+        if start_date and end_date:
+            query = query.filter((Timeseries.datetime >= start_date) & (Timeseries.datetime <= end_date))
+        if start_date:
+            query = query.filter((Timeseries.datetime >= start_date))
+        if end_date:
+            query = query.filter((Timeseries.datetime <= end_date)) 
+        else:
+            query = self.session.query(Timeseries)
+
         return [
             {
                 "id": str(q.id),
