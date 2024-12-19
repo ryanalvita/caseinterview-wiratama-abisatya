b39e5775b46d2263e0034d9cee8488dea15695f9
abisatyaw <abisatya90@gmail.com>
2024-12-19 14:28:04 +0700

    PRODUCT BACKLOG ITEM 8291 – Timeseries API: Add Date Filtering from URL Query Parameter

--- pyramid_app_caseinterview/views/api.py
+++ pyramid_app_caseinterview/views/api.py
@@ -5,9 +5,10 @@ from pyramid.view import view_config
 
 from pyramid_app_caseinterview.models.depthseries import Depthseries
 from pyramid_app_caseinterview.models.timeseries import Timeseries
+from pyramid_app_caseinterview.views.serialization import DateObject
 
 from . import View
-
+from datetime import datetime
 
 class API(View):
     """API endpoints"""
@@ -19,11 +20,30 @@ class API(View):
         request_method="GET",
     )
     def timeseries_api(self):
+        start_date = self.request.params.get("from")
+        end_date = self.request.params.get("to")
+
+        try:
+            if start_date:
+                start_date =datetime.strptime(start_date, "%Y-%m-%d")
+            if end_date:
+                end_date =datetime.strptime(end_date, "%Y-%m-%d")
+        except ValueError:
+            return {"Error" : "Invalid date format. use YYYY-MM-DD."}
+        
         query = self.session.query(Timeseries)
+        
+        if start_date and end_date:
+            query = query.filter((Timeseries.datetime >= start_date) & (Timeseries.datetime <= end_date))
+        if start_date:
+            query = query.filter((Timeseries.datetime >= start_date))
+        if end_date:
+            query = query.filter((Timeseries.datetime <= end_date)) 
+
         return [
             {
                 "id": str(q.id),
-                "datetime": q.datetime,
+                "datetime": DateObject(q.datetime),
                 "value": q.value,
             }
             for q in query.all()
